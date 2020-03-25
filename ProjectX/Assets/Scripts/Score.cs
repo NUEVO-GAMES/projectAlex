0263f891a50abad9f795fba55fd739e1891343aa
OZI-G-BE <59111911+OZI-G-BE@users.noreply.github.com>
2020-03-25 18:16:08 +0100

    new stuff

---
+++ ProjectX/Assets/Scripts/Score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+    
+     public float scorevalue;
+   public Text scoretext;
+    public Text hiscore;
+    void Start()
+    {
+        
+     hiscore.text = "BEST:" + PlayerPrefs.GetFloat("Hiscore", 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+      
+
+        scoretext.text = scorevalue.ToString();
+        if (scorevalue > PlayerPrefs.GetFloat("Hiscore", 0f))
+       { PlayerPrefs.SetFloat("Hiscore", scorevalue);
+            hiscore.text = "BEST:" + scorevalue;
+       }
+    }
+
+
+
+  
+}
---
+++ ProjectX/Assets/Scripts/count_limit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class count_limit : MonoBehaviour
+{
+  public int limit =0;
+
+  
+    public Score scoreup;
+
+void start(){
+    scoreup=FindObjectOfType<Score>();
+}
+
+      private void OnCollisionEnter2D(Collision2D other) {
+         
+     if((other.collider.tag == "Player")){
+         scoreup.scorevalue++;
+         
+        Debug.Log(scoreup.scorevalue);
+      
+         
+        }
+     
+     } 
+}
