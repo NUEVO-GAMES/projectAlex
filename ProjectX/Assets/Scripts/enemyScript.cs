3215b48a8c7b41a73a41899b313a7765c56f2cf8
BlankStardust98 <62612328+BlankStardust98@users.noreply.github.com>
2020-03-27 21:41:51 +0100

    it is working

--- ProjectX/Assets/Scripts/enemyScript.cs
+++ ProjectX/Assets/Scripts/enemyScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class enemyScript : MonoBehaviour
 {
+    public GameObject projectile;
+    // this is the attack of the enemy
     // we want the enemy to patrol on the platform which its on
     private float platformLength;
     // this is the total length of the platform in which the enemy is on 
@@ -13,16 +15,47 @@ public class enemyScript : MonoBehaviour
     // this is the direction the enmey is moving on the platform
     [SerializeField] private float speed = 1f;
     // this is the interpolation position
-  
+    [SerializeField]private RaycastHit2D ray;
+    // we have made a variable of type ray cast
+    //private Vector2 viewPointPos;
+    // this will store which side the ray should face
+    public LayerMask playerLayer;
+    // this is the layermask the player is on
+    private Vector2 bulletPath;
+    // this is the direction in which the 
+    [SerializeField] private float distance = 0.8f;
+    public bool Right;
+    // to distinguish between the rights and left of the enemys 
+    [SerializeField] float rot;
     private void Start()
     {
      
     }
     void Update()
     {
-       
+        if (GetComponent<SpriteRenderer>().flipX == true)
+        {
+            bulletPath = Vector2.right;
+        }
+        else {
+            bulletPath = Vector2.left;
+        }
+        ray = Physics2D.Raycast(transform.position, bulletPath, distance, playerLayer);
+        // this sets the ray to originate from the center of the enemy  and move to the 
+
+        if (Right == false) {
+            rot = 180f;
+            projectile.GetComponent<projectileScript>().xValue = 1;
+        }
+        else { rot = 0f;
+            projectile.GetComponent<projectileScript>().xValue = -1;
+        }
         patrol();
         // here we have declared the patrol function
+        if (ray.collider != null) {
+
+            attackMode();
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -47,6 +80,12 @@ public class enemyScript : MonoBehaviour
         }
         // the two if statements make the enemy move back anf front on the platform
     }
-  
+
+    void attackMode() 
+    {
+        if (GameObject.FindWithTag("attack")== null) { Instantiate(projectile, transform.position, new Quaternion(0, rot, 0, 0)); }
+        // this will instantiate the attack of the enemy
+    }
+
 
 }
---
+++ ProjectX/Assets/projectileScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // this a variable that will store the rigibody of the projectile
+    private float force = 10f;
+    public float xValue = 1f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        // we have referenced the rigid body of this.gameobject
+        rb.velocity = Vector2.down * 27 * Time.deltaTime;
+        // from here we make the rigid body move in a particular direction with a certain force
+       
+    }
+    private void Update()
+    {
+        transform.position += (Vector3)new Vector2(xValue, 0) * force * Time.deltaTime;
+        Destroy(this.gameObject, 1f);
+    }
+}
